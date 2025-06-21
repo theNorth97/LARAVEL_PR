fe3e55a9817398abf5e30dfe5c83d8c2fcbebbb1
Валерий Северов <on1ytrue@icloud.com>
2025-06-21 23:43:50 +0800

    v.3.9

--- app/Services/ApplicationService.php
+++ app/Services/ApplicationService.php
@@ -39,4 +39,13 @@ class ApplicationService
 
         return redirect()->route('dashboard')->with('success', 'заявка завершена');
     }
+
+    public function finish1(ActiveRequest $application)
+    {
+        $application->update([
+            'status' => 'finished',
+        ]);
+
+        return redirect()->route('dashboard')->with('success', 'заявка завершена');
+    }
 }
